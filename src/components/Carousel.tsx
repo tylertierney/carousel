b74ee6b187bc186cd6fd58306c9c697297748154
Tyler Tierney <tytierney@yahoo.com>
2022-11-14 11:07:18 -0500

    Adding custom icons support to the navigation dots

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -111,10 +111,7 @@ export const Carousel: FC<ICarouselProps> = ({
 
   return (
     <div style={style} {...props} ref={carouselRef}>
-      <div
-        className="controller"
-        style={{ display: "flex", width: "100%", position: "relative" }}
-      >
+      <div className="controller" style={{ display: "flex", width: "100%" }}>
         <div
           className="arrowContainer"
           style={{
